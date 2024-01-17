9a8c19afc2dda3478888d5ef8d7b795aeceba551
badya367 <badikov_dima@mail.ru>
2024-01-17 22:05:44 +0300

    Решено 5 задание из arrays.InPlaceOperation. Завершена глава arrays.InPlaceOperation

--- LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task5/InPlaceOperationSolutionTask5.java
+++ LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task5/InPlaceOperationSolutionTask5.java
@@ -1,4 +1,31 @@
 package org.education.leetcode.arrays.inPlaceOperations.task5;
 
 public class InPlaceOperationSolutionTask5 {
+    public int removeElement(int[] nums, int val) {
+        int quantityElements = nums.length;
+        for (int i = nums.length-1; i>=0; i--){
+            if(nums[i] == val){
+                if (i == nums.length-1){
+                    nums[i] = 0;
+                }
+                for (int j = i; j < nums.length-1; j++){
+                    nums[j] = nums[j+1];
+                }
+                quantityElements--;
+            }
+        }
+        for (int i = quantityElements; i < nums.length; i++){
+            nums[i] = 0;
+        }
+        return quantityElements;
+    }
 }
+/*
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[count++] = nums[i];
+            }
+        }
+        return count;
+ */
--- LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task5/InPlaceOperationTask5Main.java
+++ LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task5/InPlaceOperationTask5Main.java
@@ -1,4 +1,21 @@
 package org.education.leetcode.arrays.inPlaceOperations.task5;
 
+import org.education.leetcode.myGenerators.array.GeneratorArray;
+
+import java.util.Arrays;
+import java.util.Random;
+
 public class InPlaceOperationTask5Main {
+    public static void main(String[] args) {
+        int [] nums = GeneratorArray.generateArrayRandomNumWithCustomValues(10,10);
+        Random random = new Random();
+        int k = random.nextInt(11);
+        InPlaceOperationSolutionTask5 test = new InPlaceOperationSolutionTask5();
+        System.out.println("Количество элементов в массиве: " + nums.length);
+        System.out.println("Исходный массив: " + Arrays.toString(nums));
+        System.out.println("Удаляем элементы с значением: " + k);
+        int result = test.removeElement(nums,k);
+        System.out.println("Количество элементов в массиве: " + result);
+        System.out.println("Результирующий массив: " + Arrays.toString(nums));
+    }
 }
