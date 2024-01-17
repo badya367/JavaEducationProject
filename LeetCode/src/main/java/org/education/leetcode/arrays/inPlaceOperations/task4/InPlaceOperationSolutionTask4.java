50fae0450b0046d53eaf7b514b6cf6a192a05e57
badya367 <badikov_dima@mail.ru>
2024-01-17 21:42:11 +0300

    Решено 4 задание из arrays.InPlaceOperation

--- LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task4/InPlaceOperationSolutionTask4.java
+++ LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task4/InPlaceOperationSolutionTask4.java
@@ -1,4 +1,22 @@
 package org.education.leetcode.arrays.inPlaceOperations.task4;
 
 public class InPlaceOperationSolutionTask4 {
+    public int[] sortArrayByParity(int[] nums) {
+        int counter = 0;
+        for (int i = nums.length-1; i >= 0; i--) {
+            if (nums[i] % 2 == 0){
+                int changeNum = nums[i];
+                while (counter < i){
+                    if (nums[counter] % 2 != 0){
+                        nums[i] = nums[counter];
+                        nums[counter] = changeNum;
+                        counter++;
+                        break;
+                    }
+                    counter++;
+                }
+            }
+        }
+        return nums;
+    }
 }
--- LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task4/InPlaceOperationTask4Main.java
+++ LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task4/InPlaceOperationTask4Main.java
@@ -1,4 +1,12 @@
 package org.education.leetcode.arrays.inPlaceOperations.task4;
 
+import java.util.Arrays;
+
 public class InPlaceOperationTask4Main {
+    public static void main(String[] args) {
+        int[] nums = {3,1,2,4};
+        InPlaceOperationSolutionTask4 test = new InPlaceOperationSolutionTask4();
+        int[] res = test.sortArrayByParity(nums);
+        System.out.println("Результат: " + Arrays.toString(res));
+    }
 }
