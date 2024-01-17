fb30de2b92df681b2b6a9603d59f701f5ceedfe3
badya367 <badikov_dima@mail.ru>
2024-01-17 21:47:20 +0300

    Решено 4 задание из arrays.InPlaceOperation

--- LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task4/InPlaceOperationTask4Main.java
+++ LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task4/InPlaceOperationTask4Main.java
@@ -1,12 +1,15 @@
 package org.education.leetcode.arrays.inPlaceOperations.task4;
 
+import org.education.leetcode.myGenerators.array.GeneratorArray;
+
 import java.util.Arrays;
 
 public class InPlaceOperationTask4Main {
     public static void main(String[] args) {
-        int[] nums = {3,1,2,4};
+        int[] nums = GeneratorArray.generateArrayRandomNumWithCustomValues(10,20);
+        System.out.println("Сгенерированный массив: " + Arrays.toString(nums));
         InPlaceOperationSolutionTask4 test = new InPlaceOperationSolutionTask4();
-        int[] res = test.sortArrayByParity(nums);
-        System.out.println("Результат: " + Arrays.toString(res));
+        nums = test.sortArrayByParity(nums);
+        System.out.println("Результат: " + Arrays.toString(nums));
     }
 }
