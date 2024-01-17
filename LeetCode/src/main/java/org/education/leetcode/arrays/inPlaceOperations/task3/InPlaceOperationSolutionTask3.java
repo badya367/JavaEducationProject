6362d77f084ee1c0bcef304bd819a69fbd8d6355
badya367 <badikov_dima@mail.ru>
2024-01-17 15:26:11 +0300

    Решено 3 задание из arrays.InPlaceOperation

--- LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task3/InPlaceOperationSolutionTask3.java
+++ LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task3/InPlaceOperationSolutionTask3.java
@@ -1,4 +1,37 @@
 package org.education.leetcode.arrays.inPlaceOperations.task3;
 
 public class InPlaceOperationSolutionTask3 {
+    public void moveZeroes(int[] nums) {
+        int counter = nums.length-1;
+        for (int i = nums.length-1; i >= 0; i--) {
+            if (nums[i] == 0){
+                int step = i;
+                while (step != counter){
+                    nums[step] = nums[step+1];
+                    nums[step+1] = 0;
+                    step++;
+                }
+                counter--;
+
+            }
+        }
+    }
 }
+/*
+        int index = 0;
+        for (int n : nums) {
+            if (n != 0) nums[index++] = n;
+        }
+
+        while (index < nums.length) nums[index++] = 0;
+or
+        int writer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[writer];
+                nums[writer] = temp;
+                writer++;
+            }
+        }
+ */
--- LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task3/InPlaceOperationTask3Main.java
+++ LeetCode/src/main/java/org/education/leetcode/arrays/inPlaceOperations/task3/InPlaceOperationTask3Main.java
@@ -1,4 +1,15 @@
 package org.education.leetcode.arrays.inPlaceOperations.task3;
 
+import org.education.leetcode.myGenerators.array.GeneratorArray;
+
+import java.util.Arrays;
+
 public class InPlaceOperationTask3Main {
+    public static void main(String[] args) {
+        int[] nums = GeneratorArray.generateArrayRandomNumWithCustomValuesWithNegative(10,10);
+        System.out.println("Сгенерированный массив: " + Arrays.toString(nums));
+        InPlaceOperationSolutionTask3 test = new InPlaceOperationSolutionTask3();
+        test.moveZeroes(nums);
+        System.out.println("Результат: " + Arrays.toString(nums));
+    }
 }
